ff07e6779c42c2817fd46c414557b9ef6b951bef
Ravinder <solletravinder@gmail.com>
2024-08-09 15:35:39 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,55 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react'; // or '@vitejs/plugin-vue' for Vue projects
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         // Naming the main entry JavaScript file
+//         entryFileNames: 'main.js',
+//         // Naming code-split chunks
+//         chunkFileNames: 'chunks/[name].js',
+//         // Naming the main CSS file
+//         assetFileNames: ({ name, ext }) => {
+//           return '[name].[ext]'; // Default placement for other assets
+//         },
+//       },
+//     },
+//     // Optionally configure output directory
+//     outDir: 'dist',
+//   },
+//   // base: '/dummy-google-search-engine/'
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'; // or '@vitejs/plugin-vue' for Vue projects
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // Naming the main entry JavaScript file
-        entryFileNames: 'main.js',
-        // Naming code-split chunks
-        chunkFileNames: 'chunks/[name].js',
-        // Naming the main CSS file
-        assetFileNames: ({ name, ext }) => {
-          return '[name].[ext]'; // Default placement for other assets
+export default defineConfig(({ command, mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    base: isProduction ? '/dummy-google-search-engine/' : '/',
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          // Naming the main entry JavaScript file
+          entryFileNames: 'main.js',
+          // Naming code-split chunks
+          chunkFileNames: 'chunks/[name].js',
+          // Naming the main CSS file
+          assetFileNames: ({ name }) => {
+            if (/\.css$/.test(name ?? '')) {
+              return 'main.css';
+            }
+            return 'assets/[name].[ext]'; // For other assets
+          },
         },
       },
+      // Optionally configure output directory
+      outDir: 'dist',
     },
-    // Optionally configure output directory
-    outDir: 'dist',
-  },
-  base: '/dummy-google-search-engine/'
+  };
 });
+
